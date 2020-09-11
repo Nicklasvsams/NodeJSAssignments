eb4ef3981da59da86979d262d11c9648d84deba5
Nicklasvsams <nicklasvsams@gmail.com>
2020-09-11 09:50:01 +0200

    Admin functionality lists user directories and lets admin download individual users files

--- M&N Aps/M&N.js
+++ M&N Aps/M&N.js
@@ -6,7 +6,9 @@ bcrypt = require('bcrypt'), // Bcrypt is a hashing tool which is used for our pa
 saltRounds = 10, // The factor to which bcrypt encrypts the passwords - The higher the salt, the longer it takes.
 fs = require('fs'), // FileSystem to manipulate serverfiles
 formidable = require('formidable'), // Formidable is required to do formparsing
-baseDir = "C:/Users/nics/Desktop/NVS/Opgaver/Opgave 8 - Node.js/NodeOpgaver/M&N Aps"; // The base directory for the managable filesystem
+baseDir = "C:/Users/nics/Desktop/NVS/Opgaver/Opgave 8 - Node.js/NodeOpgaver/M&N Aps", // The base directory for the managable filesystem
+json = JSON.parse(fs.readFileSync('M&N Aps/users.json', (err, data) => {})),
+admin = json.admin; // Admin info from JSON
 
 // Required to read the request body
 app.use(express.urlencoded({
@@ -22,6 +24,7 @@ app.use(session({
 
 // Sends the index.html file to the client to be loaded in the browser when the base url is called
 app.get('/', (req, res) => {
+    console.log(admin.username);
     res.sendFile(baseDir + "/index.html");
 });
 
@@ -82,7 +85,7 @@ app.post('/new/users/create', (req, res) => {
 app.get('/users/login', (req, res) => {
     var username = req.query.username,
     password = req.query.password;
-
+    
     // Checks that the login parameters are valid
     if (username !== null && username !== "" && username !== undefined && password !== null && password !== "" && password !== undefined){
 
@@ -93,8 +96,16 @@ app.get('/users/login', (req, res) => {
             // Parses the json bytes into a readable string format
             let json = JSON.parse(data);
 
+            if (json.admin.username === username && bcrypt.compare(password, json.admin.password)){
+                // If the username and password matches, the session variable "user" is initialised with a new User object
+                req.session.user = new User(username, true);
+
+                // Redirects the client to the appropriate userpage
+                res.redirect(302, `/users/${req.session.user.username}`);
+            }
+
             // Checks if the users username and password matches any of the json objects
-            if (json.users.some(user => user.username === username && bcrypt.compare(password, user.password))){
+            else if (json.users.some(user => user.username === username && bcrypt.compare(password, user.password))){
 
                 // If the username and password matches, the session variable "user" is initialised with a new User object
                 req.session.user = new User(username, true);
@@ -117,40 +128,75 @@ app.get('/users/login', (req, res) => {
 // When the users/login route function has been resolved, this route function is called
 app.get('/users/:userName', (req, res) =>{
     // Initialisation of htmlDir string, which will contain information about the users directory in html format
-    htmlDir = `<h3>${req.params.userName}'s directory</h3>`;
+    htmlDir = `<h2>${req.params.userName}'s directory</h2>`;
+
     try{
         // Checks if the user session object is authenticated and if the parameter name matches the session username to avoid security breaches
         if (req.session.user.authenticated !== undefined && req.session.user.authenticated !== false && req.params.userName === req.session.user.username){
 
             // Reinitialises the User object to update the directory information
             req.session.user = new User(req.params.userName, true);
-    
-            // If the users directory exists, a forEach loop will list all directories in html with a delete button
-            if (fs.existsSync(req.session.user.dir)){
-                req.session.user.files.forEach(file => {
-                    htmlDir = htmlDir.concat(`<p>${file}</p> ${buttonHtml("/filedelete", "get", "submit", "Delete", file)}`);
-                });
-            };     
-        
+
+            // Checks if the user is admin
+            if (req.session.user.username === admin.username){
+
+                // Checks if the users directory exists
+                if (fs.existsSync(req.session.user.dir)){
+
+                    // A loop which goes through every user directory
+                    req.session.user.files.forEach(directory => {
+
+                        // Checks if a users directory exists
+                        if (fs.existsSync(`${req.session.user.dir}/${directory}`)){
+
+                            // Lists all the files in the given directory
+                            var files = fs.readdirSync(`${req.session.user.dir}/${directory}`, function(err, list) {
+                                if (err) throw err;
+                                return list;
+                            });
+
+                            // Header for the given users directory
+                            htmlDir = htmlDir.concat(`<h3>${directory}'s files</h3>`);
+
+                            // Concatenates filename, deletebutton and downloadbutton html to string
+                            files.forEach(file => {
+                                htmlDir = htmlDir.concat(`<p>${file}</p> ${deleteButtonHtml("get", "submit", directory, "Delete", file)} 
+                                ${downloadButtonHtml("get", "submit", directory, "Download", file)}`);
+                            });
+                        };
+                    });
+                };  
+            }
+            else{
+                // If the users directory exists, a forEach loop will list all directories in html with a delete button
+                if (fs.existsSync(req.session.user.dir)){
+                    req.session.user.files.forEach(file => {
+                        htmlDir = htmlDir.concat(`<p>${file}</p> ${deleteButtonHtml("get", "submit", req.session.user.username, "Delete", file)}`);
+                    });
+                };  
+            };
+            //htmlDir = htmlDir.concat(`<p>${file}</p> ${buttonHtml("/filedelete", "get", "submit", "Delete", file)}`);
+            
             // Calls the end function with different html strings, to form a simple html page
             res.end(headerHtml()+htmlDir+fileHtml()+footerHtml());
         }
         else{
             // If the session user is not authenticated or username doesn't match, the client will get an authorization error
             res.status(401).send("Not authorized.");
-        }
+        };
     }
     catch (err){
         // If an error occurs, the client will get an authorization error
+        console.log(err);
         res.status(401).send("Not authorized.");
-    }
+    };
 });
 
 // When a get request with route filedelete/:fileName is called, the callback function will use the form parameters to delete the specific file
-app.get('/filedelete/:fileName', (req, res) => {
+app.get('/filedelete/:user/:fileName', (req, res) => {
     try{
         // Deletes a specific file based on path
-        fs.unlinkSync(`${baseDir}/userfiles/${req.session.user.username}/${req.params.fileName}`);
+        fs.unlinkSync(`${baseDir}/userfiles/${req.params.user}/${req.params.fileName}`);
 
         // Logs the deletion of said file
         console.log(`${req.params.fileName} deleted by user ${req.session.user.username}`);
@@ -164,7 +210,12 @@ app.get('/filedelete/:fileName', (req, res) => {
 
         // Sends a response with a message
         res.send("An error occured.");
-    }    
+    };
+});
+
+app.get('/filedownload/:user/:fileName', (req, res) => {
+    const file = `${req.session.user.dir}/${req.params.user}/${req.params.fileName}`;
+    res.download(file);
 });
 
 // When a post function is called with /fileupload, this function will upload the specified file to the users directory
@@ -185,12 +236,12 @@ app.post('/fileupload', (req, res) => {
         fs.rename(oldpath, newpath, function (err){
             if (err) throw err;
 
-            console.log(`File created and placed in ${newpath} by ${req.session.user.username}`)
+            console.log(`File created and placed in ${newpath} by ${req.session.user.username}`);
 
             // Redirects the user to the specific userpage after uploading the file
             res.redirect(`/users/${req.session.user.username}`);
-        })
-    })
+        });
+    });
 });
 // Tells our server to listen to port 8080 for testing purposes
 app.listen(8080);
@@ -201,38 +252,47 @@ class User {
     // Declaring a constructor that is called whenever a new User is created in memory, creating the object for use on the site
     constructor(username){
         this.username = username;
-        this.dir = `${baseDir}/UserFiles/${username}`;
         this.authenticated = true;
+
+        if (this.username === "Admin"){
+            this.dir = `${baseDir}/UserFiles`;
+        }
+        else{
+            this.dir = `${baseDir}/UserFiles/${username}`;
+        };
 
         // Checks if the directory for the user exists and generates a list of the files in the directory
         if (fs.existsSync(this.dir)){
             this.files = fs.readdirSync(this.dir, function(err, list) {
                 if (err) throw err;
-                this.files = list;
                 return list;
             });
-        }
-    }
-}
+        };
+    };
+};
 
 // The functions below this comment are functions that return strings in html form, which can be used to quickly create a simple html site with modularity
 
 function headerHtml(){
     return '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Files</title></head><body>'
-}
+};
 
 function fileHtml(){
-    return '<div><form action="/fileupload" method="post" enctype="multipart/form-data"><input type="file" name="filetoupload"><br><input type="submit"></div>';
-}
+    return '<br><div><form action="/fileupload" method="post" enctype="multipart/form-data"><input type="file" name="filetoupload"><br><input type="submit"></div>';
+};
 
 function footerHtml(){
     return '</body></html>'
-}
+};
 
-function buttonHtml(action, method, type, text, variable){
-    return `<form action="${action}" method="${method}"><button type="${type}" formaction="/filedelete/${variable}">${text}</button></form>`
-}
+function downloadButtonHtml(method, type, user, text, variable){
+    return `<form action="/filedownload" method="${method}"><button type="${type}" formaction="/filedownload/${user}/${variable}">${text}</button></form>`
+};
+
+function deleteButtonHtml(method, type, user, text, variable){
+    return `<form action="/filedelete" method="${method}"><button type="${type}" formaction="/filedelete/${user}/${variable}">${text}</button></form>`
+};
 
 function informationHtml(message, action, buttonText = "Back"){
     return `<p>${message}</p> <br><form method="GET" action="${action}"><button type="submit" formaction="${action}">${buttonText}</button></form>`
-}
+};
